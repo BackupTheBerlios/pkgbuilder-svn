cbc04fcd0c5dcb2c30cea79cfbb2dbbf2d356ae2
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-10-21 19:22:57 +0000

    initial version

---
+++ trunk/pkgbuilder/common/do_build_null.sh
@@ -0,0 +1,7 @@
+# Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/common/Attic/do_build_null.sh,v 1.1 2003/10/21 19:22:57 tomby Exp $
+
+do_build() {
+    echo "build: nothing to do"
+}
