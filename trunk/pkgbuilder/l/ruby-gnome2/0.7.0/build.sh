f08284718edce811eccab6f57650f08de0ff310d
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-10-22 21:41:01 +0000

    *** empty log message ***

--- trunk/pkgbuilder/l/ruby-gnome2/0.7.0/build.sh
+++ trunk/pkgbuilder/l/ruby-gnome2/0.7.0/build.sh
@@ -1,6 +1,6 @@
 # Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/ruby-gnome2/0.7.0/Attic/build.sh,v 1.1 2003/10/21 19:31:03 tomby Exp $
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/ruby-gnome2/0.7.0/Attic/build.sh,v 1.2 2003/10/22 21:41:01 tomby Exp $
 #
 # example pkgbuilder script
 #
@@ -12,7 +12,7 @@ include do_patch
 
 include do_build
 include do_install
-include do_postinstall
+
 include do_buildpkg
 include do_installpkg
 include do_upgradepkg
@@ -24,4 +24,22 @@ do_configure() {
     ruby extconf.rb
     
     return $?
+}
+
+do_postinstall() {
+    find $PKG_DEST -exec chown root.root {} \;
+    
+    cd $PKG_SRC
+    
+    mkdir -p $PKG_DOC
+
+    cp -a $PKG_DOC_FILES $PKG_DOC
+    
+    strip_all $PKG_DEST
+
+    gzip_man $PKG_DEST$PKG_PREFIX/man
+
+    mkdir -p $PKG_DEST/install
+
+    cat $PKG_HOME/files/slack-desc > $PKG_DEST/install/slack-desc
 }
