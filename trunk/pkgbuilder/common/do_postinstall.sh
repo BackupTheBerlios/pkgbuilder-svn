c8602d0a6483e867cc402745fb585f2f11d5469a
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-10-24 19:40:40 +0000

    eliminado chmod incorrecto e innecesario

--- trunk/pkgbuilder/common/do_postinstall.sh
+++ trunk/pkgbuilder/common/do_postinstall.sh
@@ -1,6 +1,6 @@
 # Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /cvsroot/pkgbuilder/pkgbuilder/common/Attic/do_postinstall.sh,v 1.2 2003/10/22 21:47:32 tomby Exp $
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/common/Attic/do_postinstall.sh,v 1.3 2003/10/24 19:40:40 tomby Exp $
 
 do_postinstall() {
     cd $PKG_SRC
@@ -8,7 +8,6 @@ do_postinstall() {
     mkdir -p $PKG_DOC
 
     cp $PKG_DOC_FILES $PKG_DOC
-    find $PKG_DOC -exec chmod root.root {} \;
     find $PKG_DOC -type f -exec chmod 644 {} \;
     find $PKG_DOC -type d -exec chmod 755 {} \;
 
