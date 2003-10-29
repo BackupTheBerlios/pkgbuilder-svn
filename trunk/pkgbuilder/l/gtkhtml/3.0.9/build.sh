c1d91e4198dd92f2d8bfef1bac5a08f5be6bd208
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-10-29 21:56:06 +0000

    patched libtool relink

--- trunk/pkgbuilder/l/gtkhtml/3.0.9/build.sh
+++ trunk/pkgbuilder/l/gtkhtml/3.0.9/build.sh
@@ -1,6 +1,6 @@
 # Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/gtkhtml/3.0.9/Attic/build.sh,v 1.1 2003/10/28 22:22:23 tomby Exp $
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/gtkhtml/3.0.9/Attic/build.sh,v 1.2 2003/10/29 21:56:06 tomby Exp $
 #
 # gtkhtml pkgbuilder script
 #
@@ -8,7 +8,7 @@
 include do_info
 include do_fetch
 include do_uncompress_bzip2
-include do_patch
+
 include do_configure
 include do_build
 include do_install
@@ -18,16 +18,25 @@ include do_installpkg
 include do_upgradepkg
 include do_clean
 
+do_patch() {
+    cd $PKG_SRC
+    
+    mv ltmain.sh ltmain.sh.orig &&
+	sed s/relink=yes/relink=no/ ltmain.sh.orig > ltmain.sh
+    
+    return $?
+}
+
 do_postinstall() {
     cd $PKG_SRC
     
-    rmdir $PKG_DEST/usr/libexec
-    rmdir $PKG_DEST/usr/lib/gtkhtml
+    rmdir $PKG_DEST$PKG_PREFIX/libexec
     
     mkdir -p $PKG_DOC
 
     cp $PKG_DOC_FILES $PKG_DOC
     find $PKG_DOC -type f -exec chmod 644 {} \;
+    find $PKG_DOC -type d -exec chmod 755 {} \;
 
     strip_all $PKG_DEST
 
--- trunk/pkgbuilder/l/gtkhtml/3.0.9/config.sh
+++ trunk/pkgbuilder/l/gtkhtml/3.0.9/config.sh
@@ -1,11 +1,11 @@
 # Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/gtkhtml/3.0.9/Attic/config.sh,v 1.1 2003/10/28 22:22:23 tomby Exp $
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/l/gtkhtml/3.0.9/Attic/config.sh,v 1.2 2003/10/29 21:56:06 tomby Exp $
 
 PKG_NAME="gtkhtml"
 PKG_VERSION="3.0.9"
 PKG_ARCH="i486"
-PKG_BUILD="am1"
+PKG_BUILD="am2"
 PKG_EXTENSION="tar.bz2"
 PKG_FILE_NAME="$PKG_NAME-$PKG_VERSION.$PKG_EXTENSION"
 PKG_URL="ftp://ftp.gnome.org/pub/gnome/sources/$PKG_NAME/3.0/$PKG_FILE_NAME"
