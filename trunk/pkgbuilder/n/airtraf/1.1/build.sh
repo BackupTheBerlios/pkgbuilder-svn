d6e515621c832cf648084c6954b284e5d9a323ae
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-11-01 13:31:47 +0000

    *** empty log message ***

--- trunk/pkgbuilder/n/airtraf/1.1/build.sh
+++ trunk/pkgbuilder/n/airtraf/1.1/build.sh
@@ -1,6 +1,6 @@
 # Copyright 2003 Antonio G. Muñoz, tomby (AT) tomby.homemelinux.org
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /cvsroot/pkgbuilder/pkgbuilder/n/airtraf/1.1/Attic/build.sh,v 1.1 2003/10/21 18:19:51 tomby Exp $
+# $Header: /cvsroot/pkgbuilder/pkgbuilder/n/airtraf/1.1/Attic/build.sh,v 1.2 2003/11/01 13:31:47 tomby Exp $
 #
 # airtraf pkgbuilder script
 # 
@@ -8,16 +8,16 @@
 include do_info
 include do_fetch
 include do_uncompress_gzip
-include do_patch
 
-include do_install
+include do_configure_null
+
 include do_postinstall
 include do_buildpkg
 include do_installpkg
 include do_upgradepkg
 include do_clean
 
-do_configure() {
+do_patch() {
     cd $PKG_SRC/src
     
     cp Makefile.rules Makefile.rules.old
@@ -31,6 +31,16 @@ do_build() {
     cd $PKG_SRC/src
     
     make
+    
+    return $?
+}
+
+do_install() {
+    cd $PKG_SRC/src
+    
+    mkdir -p $PKG_DEST$PKG_PREFIX/bin
+    
+    cp airtraf $PKG_DEST$PKG_PREFIX/bin
     
     return $?
 }
