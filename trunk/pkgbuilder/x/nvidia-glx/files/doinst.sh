987120ffda4649e18f81c1cb89f9cd39e0ceb120
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-12-22 22:31:49 +0000

    *** empty log message ***

--- trunk/pkgbuilder/x/nvidia-glx/files/doinst.sh
+++ trunk/pkgbuilder/x/nvidia-glx/files/doinst.sh
@@ -13,3 +13,4 @@ replace usr/X11R6/include/GL/glx.h.nvidia
 replace usr/X11R6/include/GL/glxtokens.h.nvidia
 
 mv usr/X11R6/lib/modules/extensions/libglx.a usr/X11R6/lib/modules/extensions/libglx.a.xfree
+mv usr/X11R6/lib/libGL.so.1.2 usr/X11R6/lib/libGL.so.1.2.xfree
