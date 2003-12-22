f23e3b1ca0076a8e81c51bcac5819e3429cc44cb
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-12-22 22:15:18 +0000

    *** empty log message ***

--- trunk/pkgbuilder/x/nvidia-glx/files/doinst.sh
+++ trunk/pkgbuilder/x/nvidia-glx/files/doinst.sh
@@ -1,1 +1,15 @@
-( cd /usr/X11R6/lib/modules/extensions/ ; mv libglx.a libglx-xfree.a  )
+#!/bin/sh
+replace() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .nvidia`"
+
+  mv $OLD $OLD.xfree
+  mv $NEW $OLD
+}
+
+replace usr/X11R6/include/GL/glext.h.nvidia
+replace usr/X11R6/include/GL/gl.h.nvidia
+replace usr/X11R6/include/GL/glx.h.nvidia
+replace usr/X11R6/include/GL/glxtokens.h.nvidia
+
+mv usr/X11R6/lib/modules/extensions/libglx.a usr/X11R6/lib/modules/extensions/libglx.a.xfree
