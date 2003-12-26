e7047e4456b6d17f85f947b4a777ae53181770ca
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-12-26 01:28:16 +0000

    *** empty log message ***

--- trunk/pkgbuilder/n/postfix/files/doinst.sh
+++ trunk/pkgbuilder/n/postfix/files/doinst.sh
@@ -6,6 +6,16 @@
 ( chmod 2711 /usr/sbin/postdrop )
 ( chmod 2711 /usr/sbin/postqueue )
 ( chmod 0600 /etc/postfix/saslpass )
+( chown root.postfix /var/spool/postfix )
+( chown postfix.postfix /var/spool/postfix/* )
+( chgrp postdrop /var/spool/postfix/maildrop )
+( chgrp postdrop /var/spool/postfix/public )
+( chmod 755 /var/spool/postfix )
+( chmod 700 /var/spool/postfix/* )
+( chmod 730 /var/spool/postfix/maildrop )
+( chmod 755 /var/spool/postfix/pid )
+( chmod 710 /var/spool/postfix/public )
+( chmod 755 /var/spool/postfix/tmp )
 ( /usr/sbin/postconf -c "/etc/postfix" -e \
         "alias_maps=hash:/etc/mail/aliases" \
         "alias_database=hash:/etc/mail/aliases" \
