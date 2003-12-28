017d52f96e2330d112117769b2fcad122919ee57
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2003-12-28 20:40:49 +0000

    *** empty log message ***

---
+++ trunk/pkgbuilder/ap/docbook-sgml-dtd/files/doinst.sh
@@ -0,0 +1,16 @@
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-3.0.cat \
+/usr/share/sgml/docbook/sgml-dtd-3.0/catalog )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-3.0.cat \
+/etc/sgml/sgml-docbook.cat )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-3.1.cat \
+/usr/share/sgml/docbook/sgml-dtd-3.1/catalog )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-3.1.cat \
+/etc/sgml/sgml-docbook.cat )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-4.0.cat \
+/usr/share/sgml/docbook/sgml-dtd-4.0/catalog )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-4.0.cat \
+/etc/sgml/sgml-docbook.cat )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-4.1.cat \
+/usr/share/sgml/docbook/sgml-dtd-4.1/catalog )
+( /usr/bin/install-catalog --add /etc/sgml/sgml-docbook-dtd-4.1.cat \
+/etc/sgml/sgml-docbook.cat )
