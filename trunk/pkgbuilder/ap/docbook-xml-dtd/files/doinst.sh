760d6ce68d8172cfc3b2aa7acd0cb9d55db9cb2b
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2004-01-03 16:15:36 +0000

    *** empty log message ***

--- trunk/pkgbuilder/ap/docbook-xml-dtd/files/doinst.sh
+++ trunk/pkgbuilder/ap/docbook-xml-dtd/files/doinst.sh
@@ -55,6 +55,68 @@ if [ ! -e /etc/xml/docbook ]; then /usr/bin/xmlcatalog --noout --create \
 "http://www.oasis-open.org/docbook/" \
 "file:///etc/xml/docbook" /etc/xml/catalog
 
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Publishing//EN" \
+    "file://$isodir/iso-pub.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Greek Letters//EN" \
+    "file://$isodir/iso-grk1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Box and Line Drawing//EN" \
+    "file://$isodir/iso-box.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Greek Symbols//EN" \
+    "file://$isodir/iso-grk3.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Negated Relations//EN" \
+    "file://$isodir/iso-amsn.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Numeric and Special Graphic//EN" \
+    "file://$isodir/iso-num.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Alternative Greek Symbols//EN" \
+    "file://$isodir/iso-grk4.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Diacritical Marks//EN" \
+    "file://$isodir/iso-dia.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Monotoniko Greek//EN" \
+    "file://$isodir/iso-grk2.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Arrow Relations//EN" \
+    "file://$isodir/iso-amsa.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Ordinary//EN" \
+    "file://$isodir/iso-amso.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Russian Cyrillic//EN" \
+    "file://$isodir/iso-cyr1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES General Technical//EN" \
+    "file://$isodir/iso-tech.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Delimiters//EN" \
+    "file://$isodir/iso-amsc.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Latin 1//EN" \
+    "file://$isodir/iso-lat1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Binary Operators//EN" \
+    "file://$isodir/iso-amsb.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Latin 2//EN" \
+    "file://$isodir/iso-lat2.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Relations//EN" \
+    "file://$isodir/iso-amsr.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Non-Russian Cyrillic//EN" \
+    "file://$isodir/iso-cyr2.ent" /etc/xml/docbook
+
+xmlcatalog --noout --add "delegatePublic" \
+    "ISO 8879:1986" \
+    "file:///etc/xml/docbook" $ROOTCATALOG
+
 if [ ! -e /etc/xml/docbook ]; then /usr/bin/xmlcatalog --noout --create \
 /etc/xml/docbook; fi &&
 /usr/bin/xmlcatalog --noout --add "public" \
@@ -109,6 +171,70 @@ if [ ! -e /etc/xml/docbook ]; then /usr/bin/xmlcatalog --noout --create \
 /usr/bin/xmlcatalog --noout --add "delegateURI" \
 "http://www.oasis-open.org/docbook/" \
 "file:///etc/xml/docbook" /etc/xml/catalog
+
+isodir=/usr/share/xml/docbook/xml-dtd-4.1.2/ent
+
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Publishing//EN" \
+    "file://$isodir/iso-pub.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Greek Letters//EN" \
+    "file://$isodir/iso-grk1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Box and Line Drawing//EN" \
+    "file://$isodir/iso-box.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Greek Symbols//EN" \
+    "file://$isodir/iso-grk3.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Negated Relations//EN" \
+    "file://$isodir/iso-amsn.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Numeric and Special Graphic//EN" \
+    "file://$isodir/iso-num.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Alternative Greek Symbols//EN" \
+    "file://$isodir/iso-grk4.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Diacritical Marks//EN" \
+    "file://$isodir/iso-dia.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Monotoniko Greek//EN" \
+    "file://$isodir/iso-grk2.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Arrow Relations//EN" \
+    "file://$isodir/iso-amsa.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Ordinary//EN" \
+    "file://$isodir/iso-amso.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Russian Cyrillic//EN" \
+    "file://$isodir/iso-cyr1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES General Technical//EN" \
+    "file://$isodir/iso-tech.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Delimiters//EN" \
+    "file://$isodir/iso-amsc.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Latin 1//EN" \
+    "file://$isodir/iso-lat1.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Binary Operators//EN" \
+    "file://$isodir/iso-amsb.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Latin 2//EN" \
+    "file://$isodir/iso-lat2.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Added Math Symbols: Relations//EN" \
+    "file://$isodir/iso-amsr.ent" /etc/xml/docbook
+xmlcatalog --noout --add "public" \
+    "ISO 8879:1986//ENTITIES Non-Russian Cyrillic//EN" \
+    "file://$isodir/iso-cyr2.ent" /etc/xml/docbook
+
+xmlcatalog --noout --add "delegatePublic" \
+    "ISO 8879:1986" \
+    "file:///etc/xml/docbook" /etc/xml/catalog
 
 ( /usr/bin/install-catalog --add /etc/sgml/xml-docbook-dtd-4.1.cat \
 /usr/share/sgml/docbook/xml-dtd-4.1/catalog )
