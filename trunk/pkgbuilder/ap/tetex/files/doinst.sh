69a67551e7425dc761751f6ec65390c6a63f6b1e
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2005-02-26 23:07:47 +0000

    fixed url

--- trunk/pkgbuilder/ap/tetex/files/doinst.sh
+++ trunk/pkgbuilder/ap/tetex/files/doinst.sh
@@ -1,9 +1,9 @@
-/usr/bin/mktexlsr &>/dev/null
-/usr/bin/texconfig init &>/dev/null
-/usr/bin/texconfig confall &>/dev/null
-/usr/bin/texconfig font rw &>/dev/null
-/usr/bin/texconfig font vardir /var/cache/fonts &>/dev/null
-/usr/bin/texconfig font options varfonts &>/dev/null
-/usr/bin/updmap &>/dev/null
-/usr/bin/fmtutil --missing &>/dev/null
-/usr/bin/texconfig font ro &>/dev/null
+usr/bin/mktexlsr &> /dev/null
+usr/bin/texconfig init &> /dev/null
+usr/bin/texconfig confall &> /dev/null
+usr/bin/texconfig font rw &> /dev/null
+usr/bin/texconfig font vardir var/cache/fonts &> /dev/null
+usr/bin/texconfig font options varfonts &> /dev/null
+usr/bin/updmap &> /dev/null
+usr/bin/fmtutil --missing &> /dev/null
+usr/bin/texconfig font ro &> /dev/null
