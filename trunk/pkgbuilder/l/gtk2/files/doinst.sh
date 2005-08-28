066e9b4dc5047f419cf3fd235f665aaf6f7f0b25
tomby <tomby@ed866ae4-cfd4-0310-a7ae-e231f5181aee>
2005-08-28 11:48:18 +0000

    gtkglext libcroco librsvg gtk2 update

--- trunk/pkgbuilder/l/gtk2/files/doinst.sh
+++ trunk/pkgbuilder/l/gtk2/files/doinst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-( gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders )
-( gtk-query-immodules-2.0 >	etc/gtk-2.0/gtk.immodules )
+( usr/bin/gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders )
+( usr/bin/gtk-query-immodules-2.0  > etc/gtk-2.0/gtk.immodules )
--- trunk/pkgbuilder/l/librsvg/files/doinst.sh
+++ trunk/pkgbuilder/l/librsvg/files/doinst.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ( usr/bin/gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders )
+( usr/bin/gtk-query-immodules-2.0  > etc/gtk-2.0/gtk.immodules )
